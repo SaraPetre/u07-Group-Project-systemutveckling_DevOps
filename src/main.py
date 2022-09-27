9d304f4b99bd72e9d01198ef05a5c11c2e366352
lura00 <ludvig.skinstad@gmail.com>
2022-09-27 11:52:46 +0200

    written some tests to check status code and that our scrape from onedev api does not return an empty list

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ def collect_s():
     for job in result.json():
         if job['status'] == "SUCCESSFUL" :
             successful_builds += 1 
-    return successful_builds  
+    return successful_builds
         
 def collect_f():
     result = requests.get('http://localhost:6610/api/builds?offset=0&count=20',auth=HTTPBasicAuth('admin', 'admin'))
@@ -20,7 +20,7 @@ def collect_f():
     return fail_builds   
         
 def collect_p():
-    result = requests.get('http://localhost:6610/api/projects?offset=0&count=20',auth=HTTPBasicAuth('arash', 'aghaleo1'))
+    result = requests.get('http://localhost:6610/api/projects?offset=0&count=20',auth=HTTPBasicAuth('admin', 'admin'))
     projects = 0
     for project in result.json():
         if project["id"] != 0 :
